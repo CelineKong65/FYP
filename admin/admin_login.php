38d4e1f070098d7e50144fd3df88bb3bc6e93be9
PAN ZHI XIN <1221207256@soffice.mmu.edu.my>
2025-03-06 00:51:42 +0800

    product_view

--- admin/admin_login.php
+++ admin/admin_login.php
@@ -20,7 +20,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             if ($password === $admin['AdminPassword']) {
                 $_SESSION['AdminID'] = $admin['AdminID'];
                 $_SESSION['admin_username'] = $admin['AdminName'];
-                header("Location: category_view.php");
+                header("Location: product_view.php");
                 exit();
             } else {
                 echo "<script>alert('Incorrect password.'); window.location.href='admin_login.php';</script>";
